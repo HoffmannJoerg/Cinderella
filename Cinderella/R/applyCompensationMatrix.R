b5b093a161dff44db0da71f236099ec376cab6e2
HoffmannJoerg <111962108+HoffmannJoerg@users.noreply.github.com>
2022-08-24 11:21:46 +0200

    Delete Cinderella directory

--- Cinderella/R/applyCompensationMatrix.R
+++
@@ -1,6 +0,0 @@
-applyCompensationMatrix = function(Data, Compensation, Columns){
-  DataToCompensate = Data[,Columns]
-  CompensatedData = DataToCompensate %*% Compensation
-  Data[,Columns] = CompensatedData
-  return(Data)
-}
